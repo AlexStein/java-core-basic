c54c4baa06fa5e9230187dcc8de9b563dbfa37a9
Alex Shevelev <alex.s@softmine.ru>
2020-08-06 09:51:29 +0300

    * Added package zoo with classes Animal, Cat, Dog for lesson 6 homework

---
+++ src/zoo/animals/Animal.java
@@ -0,0 +1,54 @@
+package zoo.animals;
+
+public class Animal {
+
+    static int animalCounter = 0;
+    static int catCounter = 0;
+    static int dogCounter = 0;
+
+    String name;
+    protected int maxSwimDistance;
+    protected int maxRunDistance;
+
+    Animal(String name) {
+        animalCounter++;
+
+        this.name = name;
+        maxRunDistance = 0;
+        maxSwimDistance = 0;
+    }
+
+    /**
+     * Вывод счетсчика созданных объектов
+     */
+    public static void printObjectCounter() {
+        System.out.printf("Создано %d объектов класса Animal, %d Cat, %d Dog",
+                animalCounter, catCounter,dogCounter);
+    }
+
+    public void run(int distance) {
+        if (distance <= 0) {
+            System.out.printf("%s никуда не побежал\n", name);
+            return;
+        }
+        String tired = "";
+        if (distance > maxRunDistance) {
+            tired = " и устал";
+        }
+
+        System.out.printf("%s пробежал %dм%s.\n", name, Math.min(distance, maxRunDistance), tired);
+    }
+
+    public void swim(int distance) {
+        if (distance <= 0) {
+            System.out.printf("%s никуда не поплыл\n", name);
+            return;
+        }
+        String drown = "";
+        if (distance > maxSwimDistance) {
+            drown = " и утонул";
+        }
+
+        System.out.printf("%s проплыл %dм%s.\n", name, Math.min(distance, maxSwimDistance), drown);
+    }
+}
